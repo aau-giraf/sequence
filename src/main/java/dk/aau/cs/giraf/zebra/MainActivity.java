fa742c6f072d5dea645fa12a62c24eee746aa77f
Joakim Iversen <jiver12@student.aau.dk>
2015-04-15 08:43:20 +0200

    clean up mainactivity and manifest

--- src/main/java/dk/aau/cs/giraf/zebra/MainActivity.java
+++ src/main/java/dk/aau/cs/giraf/zebra/MainActivity.java
@@ -19,11 +19,16 @@ import dk.aau.cs.giraf.oasis.lib.Helper;
 import dk.aau.cs.giraf.oasis.lib.models.Profile;
 import dk.aau.cs.giraf.oasis.lib.models.Sequence;
 
+/*
+ * This is the main activity of the sequence application
+ * The activity shows the overview page, of available sequences, for the chosen user
+ */
 public class MainActivity extends GirafActivity {
 
     private Profile guardian;
     private Profile selectedChild;
     private boolean childIsSet = false;
+    private int guardianId;
     private int childId;
 
     private GridView sequenceGrid;
@@ -43,25 +48,27 @@ public class MainActivity extends GirafActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Creating buttons
+        // Creating buttons for the action bar
         changeUserButton = new GirafButton(this, getResources().getDrawable(R.drawable.icon_change_user));
         addButton = new GirafButton(this, getResources().getDrawable(R.drawable.icon_add));
         copyButton = new GirafButton(this, getResources().getDrawable(R.drawable.icon_copy));
         deleteButton = new GirafButton(this, getResources().getDrawable(R.drawable.icon_delete));
 
-        // Adding buttons
+        // Adding buttons to the action bar
         addGirafButtonToActionBar(changeUserButton, LEFT);
         addGirafButtonToActionBar(addButton, RIGHT);
         addGirafButtonToActionBar(deleteButton, RIGHT);
 
-        // Setup additional things
+        // Setup the sequence grid view used to display sequences
         setupSequenceGridView();
+        // Setup the buttons (onClickListener)
         setupButtons();
+        // Setup mode, this is either citizen mode or guardian mode
         setupModeFromIntents();
     }
 
+    //Sets the GridView and adapter to display Sequences
     private void setupSequenceGridView() {
-        //Sets the GridView and adapter to display Sequences
         sequenceGrid = (GridView) findViewById(R.id.sequence_grid);
         sequenceAdapter = new SequenceListAdapter(this, sequences);
         sequenceGrid.setAdapter(sequenceAdapter);
@@ -69,37 +76,7 @@ public class MainActivity extends GirafActivity {
 
     // Creates all buttons for the Activity and their listeners.
     private void setupButtons() {
-        addButton.setOnClickListener(new OnClickListener() {
-            //Open AddEditSequencesActivity when clicking the Add Button
-            @Override
-            public void onClick(View v) {
-                Sequence sequence = new Sequence();
-                enterSequence(sequence, true);
-            }
-        });
 
-        deleteButton.setOnClickListener(new OnClickListener() {
-            //Open DeleteSequencesActivity when clicking the delete Button
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplication(), DeleteSequencesActivity.class);
-                intent.putExtra("childId", selectedChild.getId());
-                startActivity(intent);
-            }
-        });
-
-        copyButton.setOnClickListener(new OnClickListener() {
-            //Open CopySequencesActivity when clicking the Copy Button
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplication(), CopySequencesActivity.class);
-                intent.putExtra("childId", selectedChild.getId());
-                intent.putExtra("guardianId", guardian.getId());
-                startActivity(intent);
-            }
-        });
-
-        // Click event for change user button
         changeUserButton.setOnClickListener(new OnClickListener() {
             //Open Child Selector when pressing the Child Select Button
             @Override
@@ -118,17 +95,46 @@ public class MainActivity extends GirafActivity {
                 });
             }
         });
+
+        addButton.setOnClickListener(new OnClickListener() {
+            //Open AddEditSequencesActivity when clicking the Add Button
+            @Override
+            public void onClick(View v) {
+                Sequence sequence = new Sequence();
+                enterAddEditSequence(sequence, true);
+            }
+        });
+
+        copyButton.setOnClickListener(new OnClickListener() {
+            //Open CopySequencesActivity when clicking the Copy Button
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), CopySequencesActivity.class);
+                intent.putExtra("childId", selectedChild.getId());
+                intent.putExtra("guardianId", guardian.getId());
+                startActivity(intent);
+            }
+        });
+
+        deleteButton.setOnClickListener(new OnClickListener() {
+            //Open DeleteSequencesActivity when clicking the delete Button
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), DeleteSequencesActivity.class);
+                intent.putExtra("childId", selectedChild.getId());
+                startActivity(intent);
+            }
+        });
     }
 
+    // Sets up either guardian mode or citizen mode, based on the intents
     private void setupModeFromIntents() {
-        //Create helper to fetch data from database
+        //Create helper to fetch data from database and fetches intents (from Launcher or AddEditSequencesActivity)
         helper = new Helper(this);
-
-        //Fetches intents (from Launcher or AddEditSequencesActivity)
         Bundle extras = getIntent().getExtras();
 
         //Get GuardianId and ChildId from extras
-        int guardianId = extras.getInt("currentGuardianID");
+        guardianId = extras.getInt("currentGuardianID");
         childId = extras.getInt("currentChildID");
 
         //Save guardian locally (Fetch from Database by Id)
@@ -147,21 +153,9 @@ public class MainActivity extends GirafActivity {
         }
     }
 
-    private synchronized void setChild() {
-        //Creates helper to fetch data from the Database
-        helper = new Helper(this);
-        
-        //Save Child locally and update relevant information for application
-        selectedChild = helper.profilesHelper.getProfileById(childId);
-        this.setActionBarTitle(getResources().getString(R.string.app_name)); // selectedChild.getName() "Child's name code"
-
-        // AsyncTask thread
-        AsyncFetchDatabase fetchDatabaseSetChild = new AsyncFetchDatabase();
-        fetchDatabaseSetChild.execute();
-    }
-
+    // Sets up guardian mode - all functions are enabled
     private void setupGuardianMode() {
-        //Clicking a Sequence lifts up the view and leads up to entering AddEditSequencesActivity by calling enterSequence
+        //Clicking a Sequence lifts up the view and leads up to entering AddEditSequencesActivity by calling enterAddEditSequence
         sequenceGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
@@ -169,11 +163,33 @@ public class MainActivity extends GirafActivity {
                 ((PictogramView) view).liftUp();
                 Sequence sequence = sequenceAdapter.getItem(position);
                 // Intent is not stated here, as there are two different modes - if guardian then edit mode, else if citizen then view mode
-                enterSequence(sequence, false);
+                enterAddEditSequence(sequence, false);
             }
         });
     }
 
+    // Used to select a child
+    private void pickAndSetChild() {
+        //Create helper to fetch data from database
+        helper = new Helper(this);
+
+        //Create ProfileSelector to make Guardian select Child
+        final GProfileSelector childSelector = new GProfileSelector(this, guardian, null, false);
+
+        //When child is selected, save Child locally and update application accordingly (Title name and Sequences)
+        childSelector.setOnListItemClick(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                childId = (int) id;
+                setChild();
+                childIsSet = true;
+                childSelector.dismiss();
+            }
+        });
+        childSelector.show();
+    }
+
+    // Sets up child mode - only possible to view sequences
     private void setupChildMode() {
         //When clicking a Sequence, lift up the view, create Intent for SequenceViewer and launch it
         sequenceGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -199,28 +215,23 @@ public class MainActivity extends GirafActivity {
         changeUserButton.setVisibility(View.INVISIBLE);
     }
 
-    private void pickAndSetChild(){
-        //Create helper to fetch data from database
+    // Collects data about the child - and set actionbar title
+    private synchronized void setChild() {
+        //Creates helper to fetch data from the Database
         helper = new Helper(this);
 
-        //Create ProfileSelector to make Guardian select Child
-        final GProfileSelector childSelector = new GProfileSelector(this, guardian, null, false);
+        //Save Child locally and update relevant information for application
+        selectedChild = helper.profilesHelper.getProfileById(childId);
+        this.setActionBarTitle(getResources().getString(R.string.app_name)); // selectedChild.getName() "Child's name code"
 
-        //When child is selected, save Child locally and update application accordingly (Title name and Sequences)
-        childSelector.setOnListItemClick(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                childId = (int) id;
-                setChild();
-                childIsSet = true;
-                childSelector.dismiss();
-            }
-        });
-        childSelector.show();
+        // AsyncTask thread
+        AsyncFetchDatabase fetchDatabaseSetChild = new AsyncFetchDatabase();
+        fetchDatabaseSetChild.execute();
     }
 
     //Sets up relevant intents and starts AddEditSequencesActivity
-    private void enterSequence(Sequence sequence, boolean isNew) {
+    private void enterAddEditSequence(Sequence sequence, boolean isNew) {
+
         Intent intent = new Intent(getApplication(), AddEditSequencesActivity.class);
         intent.putExtra("childId", selectedChild.getId());
         intent.putExtra("guardianId", guardian.getId());
@@ -228,11 +239,6 @@ public class MainActivity extends GirafActivity {
         intent.putExtra("isNew", isNew);
         intent.putExtra("sequenceId", sequence.getId());
         startActivity(intent);
-    }
-
-    // Finishes the current activity
-    private void finishActivity() {
-        finish();
     }
 
     // AsyncTask. Used to fetch data from the database in another thread which is NOT the GUI thread
